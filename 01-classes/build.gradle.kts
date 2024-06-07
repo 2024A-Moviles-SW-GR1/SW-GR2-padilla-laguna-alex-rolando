10eb307f0c75c20974b8d9f0e9ba4be0048d692e
Alex Padilla <alex.padilla@epn.edu.ec>
2024-06-06 23:27:08 -0500

    feat: upload project

---
+++ 01-classes/build.gradle.kts
@@ -0,0 +1,12 @@
+buildscript {
+    dependencies {
+        classpath(libs.gradle)
+        classpath(libs.google.services)
+        classpath(libs.firebase.appdistribution.gradle)
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+}
---
+++ 01-classes/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "2024ASwGr1ARPL"
+include(":app")
